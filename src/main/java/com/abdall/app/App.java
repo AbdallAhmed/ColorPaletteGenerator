384dec27725870ed8770f9cccd4aa5cbbd81099e
AbdallAhmed <ahmed.abdall9@gmail.com>
2020-05-30 18:06:07 -0400

    Added own classification and cleaned up functions

--- src/main/java/com/abdall/app/App.java
+++ src/main/java/com/abdall/app/App.java
@@ -5,14 +5,21 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
+import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
+
 import javax.imageio.ImageIO;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
 
 import de.androidpit.colorthief.ColorThief;
 
@@ -22,11 +29,12 @@ import de.androidpit.colorthief.ColorThief;
 public class App extends PApplet {
 
     PImage img;
-    String imageString = "./resources/Images/IMG_0140.jpg";
+    String imageString = "./resources/Images/IMG_0335.jpg";
     int[][] pal;
+    int[][] pal2;
     int majorAxis = 1080;
     int squareAxis = 1200;
-    int nonMajorAxis; 
+    int nonMajorAxis;
 
     public static void main() {
         PApplet.main("App");
@@ -45,42 +53,20 @@ public class App extends PApplet {
             e.printStackTrace();
         }
 
-        pal = ColorThief.getPalette(source, 10, 1, true);
-        // System.out.println("******Before grooming*************");
-        // for (int[] a : pal) {
-        //     for (int i : a) {
-        //         System.out.print(i + " ");
-        //     }
-        //     System.out.println();
-        // }
-        // thinArrayTwo(pal);
-        //pal = thinArray(pal);
-
-        int[][] finalColorSet = new int[10][3];
-
-        int count = 0;
-        for (int i = 0; i < pal.length; i++) {
-            if (pal[i][0] + pal[i][1] + pal[i][2] != (255 * 3)) {
-                finalColorSet[count][0] = pal[i][0];
-                finalColorSet[count][1] = pal[i][1];
-                finalColorSet[count][2] = pal[i][2];
-                count++;
-            }
-        }
-
-        System.out.println(count);
-        pal = finalColorSet;
+        pal = ColorThief.getPalette(source, 30, 1, true);
+        pal = thinArray(pal);
 
         img = loadImage(imageString);
-        if(img.width > img.height){
+        if (img.width > img.height) {
             img.resize(majorAxis, 0);
             nonMajorAxis = img.height;
-        }
-        else{
+        } else {
             img.resize(0, majorAxis);
             nonMajorAxis = img.width;
         }
-        
+
+        //pal = ownPaletteApproach(1000);
+
     }
 
     public void setup() {
@@ -90,42 +76,41 @@ public class App extends PApplet {
 
     public void draw() {
         noLoop();
- 
+
         imageMode(CENTER);
-        if(img.width > img.height){
-            image(img, squareAxis/2, (squareAxis/2)-80);
-        }
-        else{
-            image(img, (squareAxis/2)-80, squareAxis/2);
+        if (img.width > img.height) {
+            image(img, squareAxis / 2, (squareAxis / 2) - 80);
+        } else {
+            image(img, (squareAxis / 2) - 80, squareAxis / 2);
         }
 
         noStroke();
         rectMode(CORNER);
 
-        //major axis minus the excess minus the padding between each rectangle
-        //this is the size of the entire block next to the image (minus one rectangle)
+        //major axis minus the padding between each rectangle
+        //this is the size of the entire block next to the image (minus onerectangle)
         //so divide by # of colors
-        int rectangleVariableDim = (majorAxis - (10*(pal.length-1))) /pal.length; 
+        int rectangleVariableDim = (majorAxis - (10*(pal.length-1))) /pal.length;
 
         //center of the image is 80 pixels up from the half
         //add half the image non-major axis to the middle of it
         //give a 10 pixel buffer
-        int rectangleVariablPos =  ((squareAxis/2)-80) + (nonMajorAxis / 2) + 10;
+        int rectangleVariablPos = ((squareAxis/2)-80) + (nonMajorAxis / 2) + 10;
 
         int fixedDim = 150;
         for (int i = 0; i < pal.length; i++) {
-            fill(pal[i][0], pal[i][1], pal[i][2]);
+        fill(pal[i][0], pal[i][1], pal[i][2]);
 
-            //start from the edge of the image and build out (+10 for the spacing)
-            int movingPos = ((squareAxis - majorAxis) / 2)+((rectangleVariableDim+10)*i);
-            
-            if(img.width > img.height){
-                rect(movingPos, rectangleVariablPos, rectangleVariableDim, fixedDim);
-            }
-            else{
-                rect(rectangleVariablPos, movingPos, 150, rectangleVariableDim);
-            }
-            
+        //start from the edge of the image and build out (+10 for the spacing)
+        int movingPos = ((squareAxis - majorAxis) / 2)+((rectangleVariableDim+10)*i);
+
+        if(img.width > img.height){
+        rect(movingPos, rectangleVariablPos, rectangleVariableDim, fixedDim);
+        }
+        else{
+        rect(rectangleVariablPos, movingPos, 150, rectangleVariableDim);
+        }
+
         }
 
         save("yee-yee.jpg");
@@ -172,10 +157,22 @@ public class App extends PApplet {
             }
             distanceToPrune += 10;
         } while (arr.length - removed > 10);
-        return arr;
+
+        int[][] finalColorSet = new int[10][3];
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i][0] + arr[i][1] + arr[i][2] != (255 * 3)) {
+                finalColorSet[count][0] = arr[i][0];
+                finalColorSet[count][1] = arr[i][1];
+                finalColorSet[count][2] = arr[i][2];
+                count++;
+            }
+        }
+        return finalColorSet;
     }
 
-    public int[][] thinArrayTwo(int[][] arr) {
+    public int[][] distnaceToWhite(int[][] arr) {
         ArrayList<Double> holdDistances = new ArrayList<Double>();
         Map<Double, Integer> map = new TreeMap<Double, Integer>();
 
@@ -188,25 +185,66 @@ public class App extends PApplet {
             map.put(distance, i);
         }
         System.out.println(map);
-        
+
         int[][] smallerArray = new int[10][3];
 
-        TreeMap<Double, Integer> myNewMap = map.entrySet().stream()
-        .limit(10)
-        .collect(TreeMap::new, (m, e) -> m.put(e.getKey(), e.getValue()), Map::putAll);
+        TreeMap<Double, Integer> myNewMap = map.entrySet().stream().limit(10).collect(TreeMap::new,
+                (m, e) -> m.put(e.getKey(), e.getValue()), Map::putAll);
 
         System.out.println(myNewMap);
 
-       List<Integer> newList = new ArrayList<Integer>(myNewMap.values());
+        List<Integer> newList = new ArrayList<Integer>(myNewMap.values());
 
-       int count = 0;
-       for(Integer i : newList){
-           smallerArray[count][0] = arr[i][0];
-           smallerArray[count][1] = arr[i][1];
-           smallerArray[count][2] = arr[i][2];
-           count++;
-       }
+        int count = 0;
+        for (Integer i : newList) {
+            smallerArray[count][0] = arr[i][0];
+            smallerArray[count][1] = arr[i][1];
+            smallerArray[count][2] = arr[i][2];
+            count++;
+        }
 
-       return smallerArray; 
+        return smallerArray;
+    }
+
+    public int[][] ownPaletteApproach(int topN) {
+        img.loadPixels();
+        //int[] list = Arrays.stream(img.pixels).distinct().toArray();
+        int[] list = img.pixels;
+        // Map<Integer, Long> freqMap = Arrays.stream(img.pixels).boxed()
+        //         .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        // for (Map.Entry<Integer, Long> entry : freqMap.entrySet()) {
+        //     if (entry.getValue() > 10000) {
+        //         int stuffed = entry.getKey();
+        //         float red = stuffed >> 16 & 0xFF;
+        //         float green = stuffed >> 8 & 0xFF;
+        //         float blue = stuffed & 0xFF;
+
+        //         System.out.println(red + " " + green + " " + blue);
+        //     }
+        // }
+
+        Multiset<Integer> numberMap = HashMultiset.create();
+        for(int i : list){
+            numberMap.add(i);
+        }
+
+        List<Integer> test = Multisets.copyHighestCountFirst(numberMap).elementSet().asList().subList(0, topN);
+        int[][] smallerArray = new int[topN][3];
+        int count = 0;
+        for(int i : test){
+            float red = i >> 16 & 0xFF;
+            float green = i >> 8 & 0xFF;
+            float blue = i & 0xFF;
+
+            smallerArray[count][0] = Math.round(red);
+            smallerArray[count][1] = Math.round(green);
+            smallerArray[count][2] = Math.round(blue);
+            count++;
+            System.out.println(red + "," + green + "," + blue);
+        }
+        pal = thinArray(smallerArray);
+ 
+        return pal;
     }
 }
