4ad4a1fa860c137dd5ca812a98575caed3e2af7e
AbdallAhmed <ahmed.abdall9@gmail.com>
2021-12-26 12:19:08 -0500

    pushing all current features

--- src/main/java/com/abdall/app/App.java
+++ src/main/java/com/abdall/app/App.java
@@ -15,7 +15,7 @@ import de.androidpit.colorthief.ColorThief;
 public class App extends PApplet {
 
     PImage img;
-    String imageString = "./resources/Images/IMG_0335.jpg";
+    String imageString = "./resources/Images/IMG_0140.jpg";
     int[][] pal;
     int[][] pal2;
     int majorAxis = 1080;
@@ -39,8 +39,8 @@ public class App extends PApplet {
             e.printStackTrace();
         }
 
-        pal = ColorThief.getPalette(source, 30, 1, true);
-        pal = Utility.thinArray(pal);
+        // pal = ColorThief.getPalette(source, 30, 1, true);
+        // pal = Utility.thinArray(pal);
         
         img = loadImage(imageString);
         if (img.width > img.height) {
@@ -50,8 +50,8 @@ public class App extends PApplet {
             img.resize(0, majorAxis);
             nonMajorAxis = img.width;
         }
-
-        // pal = Utility.ownPaletteApproach(img,1000);
+        
+        pal = Utility.ownPaletteApproach(img,100);
         // pal = Utility.distanceToWhite(pal);
         // img.loadPixels();;
         // int[] list = img.pixels;
